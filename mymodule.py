c4fd24ead66134bbfa4b3d0fbfd83a0e090a400e
alishasajjad <dev.alishasajjad@gmail.com>
2026-07-10 07:49:09 +0500

    Add mymodule for Python Day 3

---
+++ mymodule.py
@@ -0,0 +1,16 @@
+def greeting(name):
+    print("Hello", name)
+
+person = {
+    "name": "Alisha",
+    "age": 32,
+    "passed": True
+}
+
+name = "Ali"
+
+marks = [80,90,70]
+
+def add(a,b):
+    return a+b
+
